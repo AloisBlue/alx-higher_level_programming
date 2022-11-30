506ad6b07ca8a994d2eea8d906ba85ce7d575726
AloisBlue <aloismburu@gmail.com>
2022-11-30 05:45:27 -0800

    Argumentss

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,14 +2,11 @@
 const process = require('node:process');
 
 const argvLength = process.argv.length;
-let message = '';
 
 if (argvLength === 2) {
-  message = 'No argument';
+  console.log('No argument');
 } else if (argvLength === 3) {
-  message = 'Argument found';
+  console.log('Argument found');
 } else {
-  message = 'Arguments found';
+  console.log('Arguments found');
 }
-
-console.log(message);
