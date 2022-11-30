42565edebb361b81eac2e175f8eca6996751ab1e
AloisBlue <aloismburu@gmail.com>
2022-11-30 06:53:46 -0800

    Second largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const process = require('node:process');
+let values = [];
+const pathArg = process.argv;
+
+pathArg.forEach((ag, i) => {
+  if (i > 1) {
+    values.push(ag);
+  }
+});
+
+if (values.length > 1) {
+  values.sort((a, b) => Number(b) - Number(a));
+  console.log(values[1]);    
+} else {
+  console.log(0);
+}
