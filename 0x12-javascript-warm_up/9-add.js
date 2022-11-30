57106f586cdc3b4eee5575dda2217bca569b4ea8
AloisBlue <aloismburu@gmail.com>
2022-11-30 09:48:20 -0800

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,9 @@
 const process = require('node:process');
 
 function add(a, b) {
-  console.log(Number(a) + Number(b));
+  let sum = Number(a) + Number(b);
+  console.log(sum);
+  return sum;
 };
 
 add(process.argv[2], process.argv[3]);
