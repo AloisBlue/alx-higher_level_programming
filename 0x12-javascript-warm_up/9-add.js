13074c8265fa69957d2f3e53f2ddfd3802877753
AloisBlue <aloismburu@gmail.com>
2022-11-30 09:57:28 -0800

    Semi auto

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const process = require('node:process');
 
-let { argv } = process;
-let a = Number(argv[2]);
-let b = Number(argv[3]);
+const { argv } = process;
+const a = Number(argv[2]);
+const b = Number(argv[3]);
 
-function add(a, b) {
-  let sum = a + b;
+function add (a, b) {
+  const sum = a + b;
   return sum;
-};
+}
 
 console.log(add(a, b));
