0c9ffbd2c72eec3862524d3339859a178dcb4844
AloisBlue <aloismburu@gmail.com>
2022-12-02 11:31:03 -0800

    Call me moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const callMeMoby = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   let i = 0;
-  console.log(x);
   while (i < x) {
     theFunction();
     i++;
   }
 };
-
-module.exports.callMeMoby = callMeMoby;
