ad46850878288f1853be22a4b2bc7c3d7c20a379
AloisBlue <aloismburu@gmail.com>
2022-12-01 22:57:34 -0800

    Square 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(Array(this.width).fill('X').join(''));
+    }
+  }
+
+  rotate () {
+    this.rwidth = this.width;
+    this.rheight = this.height;
+
+    this.width = this.rheight;
+    this.height = this.rwidth;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
