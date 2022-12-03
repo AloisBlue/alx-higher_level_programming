5984fb98f7ddc86260d263f913f0a55ef2d87343
AloisBlue <aloismburu@gmail.com>
2022-12-03 08:25:52 -0800

    Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,43 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const SquareSuper = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log(Array(this.width).fill('X').join(''));
-    }
-  }
-
-  rotate () {
-    this.rwidth = this.width;
-    this.rheight = this.height;
-
-    this.width = this.rheight;
-    this.height = this.rwidth;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square1 extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
-
-class Square extends Square1 {
-  constructor (square) {
-    super(square);
-    this.square = square;
-  }
+class Square extends SquareSuper {
 
   charPrint (c) {
     if (c) {
