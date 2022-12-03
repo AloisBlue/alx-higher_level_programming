913dec0c5e97a6646d997ef22c15eb7bbcb40d3d
AloisBlue <aloismburu@gmail.com>
2022-12-03 08:32:35 -0800

    Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,13 +2,10 @@
 const SquareSuper = require('./5-square');
 
 class Square extends SquareSuper {
-
   charPrint (c) {
     if (c) {
-      if (c === 'C') {
-        for (let i = 0; i < this.height; i++) {
-          console.log(Array(this.width).fill(c).join(''));
-        }
+      for (let i = 0; i < this.height; i++) {
+        console.log(Array(this.width).fill(c).join(''));
       }
     } else {
       this.print();
